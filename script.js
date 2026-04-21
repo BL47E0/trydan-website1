2041fa30fcfe6adf3d3dc40005013cbace9243c9
sujay-cj <stephenleon.george@gmail.com>
2026-04-21 19:56:41 +0530

    added ikr images

--- script.js
+++ script.js
@@ -140,4 +140,29 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 }
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+if (slides.length > 0 && nextBtn && prevBtn) {
+
+    let currentSlide = 0;
+
+    function showSlide(index) {
+        slides.forEach(slide => slide.classList.remove("active"));
+        slides[index].classList.add("active");
+    }
+
+    nextBtn.addEventListener("click", () => {
+        currentSlide = (currentSlide + 1) % slides.length;
+        showSlide(currentSlide);
+    });
+
+    prevBtn.addEventListener("click", () => {
+        currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+        showSlide(currentSlide);
+    });
+}
+
+
 });
